c1c022b0738125acd05627803f45bd6e4973792e
Akshar Patel <49744200+aksharpatel123@users.noreply.github.com>
2022-01-17 08:35:19 -0600

    Implemented add and remove
    Implemented add and remove functionalities for the notes app

--- app.js
+++ app.js
@@ -1,6 +1,64 @@
-const validator = require('validator')
-const getNotes = require('./notes')
+let chalk = require('chalk')
+const yargs = require('yargs')
+const notes = require('./notes')
 
-console.log(getNotes());
+yargs.version('1.1.0')
 
-console.log(validator.isURL('https://apple.com'))
+// creating add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+// creating remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        notes.removeNote(argv.title)
+    }
+})
+
+// creating list command
+yargs.command({
+    command: 'list',
+    describe: 'List a new note',
+    handler: function() {
+        console.log('Listing a new note...')
+    }
+})
+
+// creating read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a new note',
+    handler: function() {
+        console.log('Reading a new note...')
+    }
+})
+
+
+// console.log(yargs.argv)
+yargs.parse()
--- notes.js
+++ notes.js
@@ -1,6 +1,68 @@
+const fs = require('fs')
+let chalk = require('chalk')
+
 
 const getNotes = function() {
     return 'your notes...'
 }
 
-module.exports = getNotes
+const addNote = function(title, body) {
+    const notes = loadNotes()
+
+    const duplicateNotes = notes.filter(function (note) {
+        return note.title === title
+    })
+
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        // console.log(notes)
+        savaNotes(notes)
+        console.log(chalk.green.inverse('new note added successfully!'))
+    }
+    else {
+        console.log(chalk.red.inverse('Note title is already taken!'))
+    }
+   
+}
+
+const removeNote = function(title) {
+    const notes = loadNotes()
+
+    const notesToKeep = notes.filter(function (note) {
+        return note.title !== title
+    })
+
+    if (notes.length !== notesToKeep.length) {
+        console.log(chalk.green.inverse('Note Removed!'))
+        savaNotes(notesToKeep)
+    }
+    else {
+        console.log(chalk.red.inverse('No note found!'))
+    }
+
+    
+}
+
+const savaNotes = function(notes) {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = function () {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+    }
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote,
+    removeNote: removeNote
+}
